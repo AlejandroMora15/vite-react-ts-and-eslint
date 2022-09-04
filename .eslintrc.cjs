4055c012b4873c0f24e21d8ef6a4ffdb9ef076e4
Alejandro <legorios@gmail.com>
2022-09-04 16:29:41 -0500

    :sparkles: first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard-with-typescript'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json'
+  },
+  plugins: ['react'],
+  rules: {}
+}
